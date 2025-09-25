6698489234295ecb61ae77f11daf8078470dd827
Frenzy-FC <cuarteros.frenzcarlo123@gmail.com>
2025-09-25 21:11:44 +0800

    Update LE5_CUARTEROS,PEDROSO.c

--- LE5_CUARTEROS,PEDROSO.c
+++ LE5_CUARTEROS,PEDROSO.c
@@ -1,105 +1,25 @@
 #include <stdio.h>
 
-int main () {
+void Loop(int num, int sum) {
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &num);
 
-    int num1 = 0;
-    int num2 = 0;
-    int num3 = 0;
-    int num4 = 0;
+        if (num > 0) {
+            sum += num;
+        }
+
+    } while (num > 0);
+
+    printf("Total sum is: %d\n", sum); 
+}
+
+int main() {
+    int num = 0;
     int sum = 0;
 
-    
-        printf("Enter positive numbers to ADD (zero or negative to STOP)");
-        while(1) {
-            printf("\n\nEnter a number: ");
-            scanf("%d", &num1);
-            
-            printf("Enter a number: ");
-            scanf("%d", &num2);
-            
-            printf("Enter a number: ");
-            scanf("%d", &num3);
-            
-            printf("Enter a number: ");
-            scanf("%d", &num4);
-            
-            
-    
-            
-            if (num1 >= 1 && num2 >= 1 && num3 >= 1 && num4 >= 1){
-                sum = num1 + num2 + num3 + num4;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 >= 1 && num3 >= 1 && num4 >= 1){
-                sum = num2 + num3 + num4;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 >= 1 && num2 <= 0 && num3 >= 1 && num4 >= 1){
-                sum = num1 + num3 + num4;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 >= 1 && num2 >= 1 && num3 <= 0 && num4 >= 1){
-                sum = num1 + num2 + num4;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 >= 1 && num2 >= 1 && num3 >= 1 && num4 <= 0){
-                sum = num1 + num2 + num3;
-                printf("Total sum: %d", sum);
-                
-            }
-            else if (num1 >= 1 && num2 >= 1 && num3 <= 0 && num4 <= 0){
-                sum = num1 + num2;
-                printf("Total sum: %d", sum);
-                
-            }
-            else if (num1 >= 1 && num2 <= 0 && num3 >= 1 && num4 <= 0){
-                sum = num1 + num3;
-                printf("Total sum: %d", sum);
-                
-            }
-            else if (num1 >= 1 && num2 <= 0 && num3 <= 0 && num4 >= 1){
-                sum = num1 + num4;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 >= 1 && num3 >= 1 && num4 <= 0){
-                sum = num2 + num3;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 >= 1 && num3 <= 0 && num4 >= 1){
-                sum = num2 + num4;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 <= 0 && num3 >= 1 && num4 >= 1){
-                sum = num3 + num4;
-                printf("Total sum: %d", sum);
-                
-            }
-            else if (num1 >= 1 && num2 <= 0 && num3 <= 0 && num4 <= 0){
-                sum = num1;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 >= 1 && num3 <= 0 && num4 <= 0){
-                sum = num2;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 <= 0 && num3 >= 1 && num4 <= 0){
-                sum = num3;
-                printf("Total sum: %d", sum);
-            }
-            else if (num1 <= 0 && num2 <= 0 && num3 <= 0 && num4 >= 1){
-                sum = num4;
-                printf("Total sum: %d", sum);
-            }
-        
-        }
-        
-        
-        
-        
-        
-        
-        
+    printf("Enter positive number to ADD (zero or negative number to STOP): \n");
+    Loop(num, sum);
+
+    return 0;
 }
-        
-            
-     
